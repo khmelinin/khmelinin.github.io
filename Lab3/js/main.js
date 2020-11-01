4d4afa894920b7d99b2d14c3eb4f5e0467b921c6
khmelinin <56384437+khmelinin@users.noreply.github.com>
2020-11-02 01:23:40 +0200

    Add files via upload

--- Lab3/js/main.js
+++ Lab3/js/main.js
@@ -1,79 +1,74 @@
- "use strict";
 
- const a = 10;
- const b = 6;
- const c = 6;
- document.addEventListener('DOMContentLoaded', () => {
-  if (performance.navigation.type == 1) {
-    if(checkCookie('maxDigit')){
-      if(window.confirm(`Some Cookies have been created on this page: \n${document.cookie} \n Do you want to save them?`)) 
-      alert('Cookies were saved, now the page will be refreshed.');
-      else setCookie('maxDigit', '');
-    }
-  }
-  setCookie('session', parseInt(getCookie('session')) + 1, 1);
-
-  swapContent('block4content', 'block5content'); // complete task 1
-  countTriangle(a, b, c, 'block5'); // complete task 2
-  // task 3
-  if(checkCookie('maxDigit')) document.querySelector('#numForm').remove();
-  //complete task 6
-  makeEditableBlock('block2');
-  makeEditableBlock('block3');
-  makeEditableBlock('block5');
-  makeEditableBlock('block6');
-  initEditableBlocks();
-  // complete task 3
-  document.querySelector('#numBtn').addEventListener('click', () => {
-    let maxDigit = findMaxDigit('numInput');
-    alert(`MaxDigit digit is ${maxDigit}`);
-    setCookie('maxDigit', maxDigit, 2);
-  })
-  //complete task 4
-  if(localStorage.getItem('textColor')) {
-    document.querySelector('#block6Content').style.color = localStorage.getItem('textColor');
-    document.querySelector('#colorInput').setAttribute('value', localStorage.getItem('textColor'))
-  }
-  document.querySelector('#colorInput').addEventListener('change', function(){
-    console.log(this.value);
-    document.querySelector('#block6Content').style.color = this.value;
-    localStorage.setItem('textColor' ,this.value);
-  })
-  // complete task 5
-  document.addEventListener('select', (event) => {
-  	if(event.target == document.querySelector('#selectBlock')){
-  		const selection = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd);
-    	document.getElementById('selectLog').textContent = `You've just selected: ${selection}`;	
-  	}
-  })
-  // document.querySelector('#selectBlock').addEventListener('select', () => {
-  //   const selection = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd);
-  //   document.getElementById('selectLog').textContent = `You've just selected: ${selection}`;
-  // })
- })
-
- const swapContent = (id1, id2) => {
+// task 1
+const swap = (id1, id2) => {
   let block1 = document.getElementById(id1);
   let block2 = document.getElementById(id2);
   let content1 = block1.innerHTML;
   block1.innerHTML = block2.innerHTML;
   block2.innerHTML = content1;
  }
+swap('block4', 'block5');
 
- const countTriangle = (a, b, c, outputId) => {
+// task 2
+const countTriangle = (a, b, c, outputId) => {
   let p = (a+b+c)/2;
   let s = Math.sqrt(p*(p-a)*(p-b)*(p-c));
   document.getElementById(outputId).insertAdjacentHTML("beforeend", 
   `<p><strong>S of triangle with a=${a} b=${b} c=${c} :<br>${s}</strong></p>`
   );
  }
+countTriangle(10,6,6,'block3')
 
- const findMaxDigit = (inputId) => {
-  let number = document.getElementById(inputId).value;
-  return Array.from(number).sort()[0];
- }
+// task 3
 
- const initEditableBlocks = () => {
+const findMaxDigit = (inputId) => {
+    let number = document.getElementById(inputId).value;
+    //let arr = Array.from(number).sort();
+    let counts = 0;
+    for (var i = 0; i < Array.from(number).sort().length; i++) {
+        if(Array.from(number).sort()[i]==Array.from(number).sort()[Array.from(number).sort().length-1])
+            {
+                counts+=1;
+            }
+    }
+    return counts;
+}
+
+// task 4
+btn.onclick = function () {
+      const rbs = document.querySelectorAll('input[name="choise"]');
+      let selectedValue;
+      for (const rb of rbs) {
+          if (rb.checked) {
+              selectedValue = rb.value;
+              switch (selectedValue) {
+                case 'bolder':
+                  document.getElementById('block6').style.fontWeight='bolder';
+                  break;
+                case 'normal':
+                  document.getElementById('block6').style.fontWeight='normal';
+                  break;
+                default:
+                  document.getElementById('block6').style.fontWeight='normal';
+                  break;
+              }
+          }
+      }
+     
+  };
+
+// task 5
+onscroll = function(){
+    alert('Scrolling');
+}
+/*
+document.getElementById('textBlock').onscroll = function(){
+    alert('Scrolling');
+}
+*/
+
+//task 6
+const initialEditableBlocks = () => {
   Array.from(document.getElementsByClassName('editArea')).map((area) => {
     area.addEventListener('change', (event) => {
       const newContent = event.target.value;
@@ -89,7 +84,7 @@
   })
  }
 
- const makeEditableBlock = (blockId) => {
+ const editingBlock = (blockId) => {
    const content = localStorage.getItem(`${blockId}Content`) ? 
    localStorage.getItem(`${blockId}Content`) : 
    document.getElementById(blockId).innerHTML;
@@ -100,16 +95,33 @@
    )
  }
 
- const setCookie = (name, data, expDays) => {
+//editingBlock('block1');
+editingBlock('block2');
+editingBlock('block3');
+editingBlock('block4');
+editingBlock('block5');
+//editingBlock('block6');
+initialEditableBlocks();
+
+// cookie (task 3)
+const checkCookie = (name) => {
+    return (document.cookie.includes(name) && !document.cookie.includes(`${name}=;`));
+}
+
+const setCookie = (name, data, expDays) => {
   const d = new Date();
   d.setDate(d.getDate() + expDays);
   document.cookie = `${name}=${data};expires=${d.toUTCString()};path=/`;
- }
-
- const checkCookie = (name) => {
-  return (document.cookie.includes(name) && !document.cookie.includes(`${name}=;`));
- }
+}
 
  const getCookie = (name) => {
    return checkCookie(name) ? document.cookie.split(';').find((c) => c.includes(name)).split('=')[1] : 0;
  }
+ 
+if(checkCookie('maxDigit'))
+    document.querySelector('#numForm').remove();
+document.querySelector('#numBtn').addEventListener('click', () => {
+    let maxDigit = findMaxDigit('numInput');
+    alert(`Max digits: ${maxDigit}`);
+    setCookie('maxDigit', maxDigit, 2);
+})
